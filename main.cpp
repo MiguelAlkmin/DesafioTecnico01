84dd0a08617b86b25935f821505fd9abf434d847
MiguelAlkmin <miguelalkmin@gmail.com>
2024-03-07 15:01:11 -0300

    primeio desafio tecnico
     Querido professor acho q desenvovi depressao apos essa aula, n deu tempo terminar meu codigo, se tivesse dado tempo teria ficado uma belezinho, se eu tirar -5 n irei  me espantar, mas acho q deveria ganha pelo menos 1 por do

---
+++ main.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  //informacoes basicas do personagem 
+  string nome1 = "Arthur";
+  string nome2 = "Cervero";
+  cout << "Nome do personagem: " << nome1 << " " << nome2 << endl;
+  float altura = 1.65;
+  cout << "Altura do personagem: " << altura << endl;
+  int idade = 32;
+  cout << "Idade do personagem: " << idade << endl;
+
+  //infos do twitter do tutu
+  string usuario = "tutu_trevoso";
+  cout << "@" << usuario << endl;
+  string descricao = "ignorem os twits a seguir eu so falo asneira";
+  cout << "descricao: " << descricao << endl;
+  int seguidores = 100;
+  cout << seguidores << endl;
+  
+  return 0;
+}
